a3b0a086267bb378f9781da26a31bc146648626e
tracerwhite24 <tracerwhite@gmail.com>
2024-12-04 14:58:49 +0300

    Update keyboard_caloriebot3.py
    aiogram==2.25.1
    aiohttp==3.8.6
    aiosignal==1.3.1
    async-timeout==4.0.3
    attrs==24.2.0
    Babel==2.9.1
    certifi==2024.7.4
    charset-normalizer==3.3.2
    frozenlist==1.4.1
    idna==3.7
    magic-filter==1.0.12
    multidict==6.0.5
    pillow==10.4.0
    pytz==2024.1
    yarl==1.9.4

--- keyboard_caloriebot3.py
+++ keyboard_caloriebot3.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
 
-API = '7991163460:AAGK-HM5MXPowZNKSHAINi7SfoCEehJiMPM'
+API = ''
 
 bot = Bot(token=API)
 storage = MemoryStorage()
